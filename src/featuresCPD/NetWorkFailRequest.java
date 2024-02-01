ecd4f0b8cd69fd2a6f01b26e8491c0c610425551
MasuDeokar <mayureshdeokar@gmail.com>
2024-02-01 19:51:33 +0530

    first commit

---
+++ src/featuresCPD/NetWorkFailRequest.java
@@ -0,0 +1,35 @@
+package featuresCPD;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v121.fetch.Fetch;
+import org.openqa.selenium.devtools.v121.fetch.model.RequestPattern;
+import org.openqa.selenium.devtools.v121.network.model.ErrorReason;
+
+public class NetWorkFailRequest {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "./Server/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		DevTools devTools = driver.getDevTools();
+		devTools.createSession();
+		
+		Optional<List<RequestPattern>> patterns = Optional.of(Arrays.asList(new RequestPattern(Optional.of("*GetBook*"), Optional.empty(), Optional.empty())));
+		devTools.send(Fetch.enable(patterns, Optional.empty()));
+		
+		devTools.addListener(Fetch.requestPaused(), request ->
+		{
+			devTools.send(Fetch.failRequest(request.getRequestId(), ErrorReason.FAILED));
+		});
+		driver.get("https://rahulshettyacademy.com/angularAppdemo/");
+		driver.findElement(By.cssSelector("button[routerlink*='library']")).click();
+		
+	}
+
+}
